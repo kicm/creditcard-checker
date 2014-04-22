83aeb455afccf603d726c76bc14eebd1cea5f984
Marco Kilchhofer <marco@kilchhofer.info>
2014-04-22 18:48:35 +0200

    Fixed CheckCLI.java and CreditCard.java

--- src/creditcard/CheckCLI.java
+++ src/creditcard/CheckCLI.java
@@ -3,8 +3,6 @@ package creditcard;
 import java.util.Scanner;
 
 public class CheckCLI {
-
-<<<<<<< HEAD
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
 		CreditCard card2 = new CreditCard();
@@ -19,22 +17,4 @@ public class CheckCLI {
 		
 		in.close();
 	}
-	
-=======
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        CreditCard card2 = new CreditCard();
-
-        do {
-            System.out.println("Please enter card number:");
-            card2.setNumber(in.nextLine());
-            System.out.println("Credit Card Issuer:   " + card2.getCardType());
-            System.out.println("Luhn Algorithm Check: " + card2.checkValidity());
-            System.out.println("Continue? (y/N)");
-        } while (in.nextLine().toUpperCase().equals("Y"));
-
-        in.close();
-    }
-
->>>>>>> 8858450a49c7807d9852f621ead2ce7152edd934
 }
