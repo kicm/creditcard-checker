91df6b36063b83d2263a680e57143ec72e5342e7
Marco Kilchhofer <marco@kilchhofer.info>
2014-04-22 18:32:31 +0200

    Updated Luhn Test.

--- src/creditcard/CreditCard.java
+++ src/creditcard/CreditCard.java
@@ -1,6 +1,7 @@
 package creditcard;
 /**
- * 
+ * some numbers to check:
+ * http://www.freeformatter.com/credit-card-number-generator-validator.html
  * @author marco
  *
  */
@@ -17,15 +18,15 @@ public class CreditCard {
 	}
 	/**
 	 * Construct a credit card with the given number
-	 * @param number the number of the credit card
+	 * @param numberStr the number of the credit card
 	 */
 	public CreditCard(String numberStr) {
-		this.number = number;
+		this.number = numberStr;
 	}
 	
 	/**
 	 * Construct a credit card with the given number
-	 * @param number
+	 * @param numberInt
 	 * 			The number of the credit card as Integer
 	 */
 	public CreditCard(int numberInt) {
@@ -34,15 +35,15 @@ public class CreditCard {
 	
 	/**
 	 * Set the number of the credit card 
-	 * @param number
+	 * @param numberStr
 	 * 			The number of the credit card as a string.
 	 */
-	public void setNumber(String number) {
-		this.number = number;
+	public void setNumber(String numberStr) {
+		this.number = numberStr;
 	}
 	/**
 	 * Set the number of the credit card 
-	 * @param number
+	 * @param numberInt
 	 * 			The number of the credit card as Integer.
 	 */
 	public void setNumber(int numberInt) {
@@ -50,32 +51,29 @@ public class CreditCard {
 	}
 	/**
 	 * Performs a luhn algorithm check.
+	 * additional information: http://de.wikipedia.org/wiki/Luhn-Algorithmus#Java
+	 * 
 	 * @return true if creditcard passes Luhn Algorithm
 	 */
 	public boolean luhnTest() {
-		int s1 = 0;
-		int s2 = 0;
-
-		String reverse = new StringBuffer(this.number).reverse().toString();
-
-		for (int i = 0; i < reverse.length(); i++) {
-
-			//Use digit to provide '-1' if no numeric value
-			int digit = Character.digit(reverse.charAt(i), 10); 
-
-			// gerade digits
-			if (i % 2 == 0) {
-				s1 += digit;
-
-			} else {
-				// ungerade digits
-				s2 += 2 * digit;
-				if (digit >= 5) {
-					s2 -= 9;
-				}
-			}
+		int sum = 0;
+		int length = this.number.length();
+		for(int i = 0; i < length; i++){
+			
+			// get digits in reverse order
+			// use digit to provide '-1' if no numeric value
+			int digit = Character.digit(this.number.charAt(length - i - 1), 10);
+			
+			// every 2nd number multiply with 2
+			if (i % 2 == 1) {
+	            digit *= 2;
+	        }
+			
+			sum += digit > 9 ? digit - 9 : digit;
+			
 		}
-		return (s1 + s2) % 10 == 0;
+
+		return sum % 10 == 0;
 	}
 	
 	
@@ -100,14 +98,8 @@ public class CreditCard {
 			if (this.isDinersCarteBlanche())
 				return "Diners Club - Carte Blanche";
 
-			if (this.isDinersUSAandCanada())
-				return "Diners Club - USA & Canada";
-
 			if (this.isAmericanExpress())
 				return "American Express";
-
-			if (this.isEnRoute())
-				return "en Route";
 
 			if (this.isJCB())
 				return "JCB";
@@ -139,26 +131,35 @@ public class CreditCard {
 
 	}
 	
+	/**
+	 * @return true if card is a VISA
+	 */
 	public boolean isVisa() {
 		// VISA Check; starts with 4 ; length 13,16 ; eg. 4111 1111 1111 1111
 		return this.number.startsWith("4");
 	}
 
+	/**
+	 * @return true if card is a Master Card
+	 */
 	public boolean isMasterCard() {
 		// MasterCard Check ; starts with 51,52,53,54,55
 		// length 16; eg. 5500 0000 0000 0004
 
-		return this.number.startsWith("51") || 
+		return  this.number.startsWith("51") || 
 				this.number.startsWith("52") || 
 				this.number.startsWith("53") || 
 				this.number.startsWith("54") || 
 				this.number.startsWith("55");
 	}
 
+	/**
+	 * @return true if card is a Discover Card
+	 */
 	public boolean isDiscover() {
-		// Discover Card Check ; starts with 6011 ; length 16 ; eg. 6011 0000
-		// 0000 0004
-		// 6011, 622126 to 622925, 644, 645, 646, 647, 648, 649, 65
+		// Discover Card Check ; starts with 6011 ; 
+		// 6011, 622126 to 622925, 644, 645, 646, 647, 648, 649, 65 
+		// eg. 6011 0000 0000 0004; length 16
 		
 		
 		if ( this.number.startsWith("65")  || 
@@ -179,11 +180,14 @@ public class CreditCard {
 		return false;
 	}
 
+	/**
+	 * @return true if card is a Diners Club - Carte Blanche
+	 */
 	public boolean isDinersCarteBlanche() {
-		// Diner’s Club11 ; starts with 30,36,38 ; length 14 ; eg. 3000 0000
+		// Diners Club11 ; starts with 30,36,38 ; length 14 ; eg. 3000 0000
 		// 0000 04
 		
-		return this.number.startsWith("300") || 
+		return  this.number.startsWith("300") || 
 				this.number.startsWith("301") || 
 				this.number.startsWith("302") || 
 				this.number.startsWith("303") || 
@@ -191,31 +195,31 @@ public class CreditCard {
 				this.number.startsWith("305");
 	}
 
+	/**
+	 * @return true if card is a Diners Club - International
+	 */
 	public boolean isDinersInternational() {
 		//
 		return this.number.startsWith("36");
 	}
 
-	public boolean isDinersUSAandCanada() {
-		//
-		return this.number.startsWith("54");
-	}
 
+	/**
+	 * @return true if card is an Amex
+	 */
 	public boolean isAmericanExpress() {
 		// American Express12 34,37 15 3400 0000 0000 009
-		return this.number.startsWith("34") || 
+		// Length 15
+		return  this.number.startsWith("34") || 
 				this.number.startsWith("37");
 	}
 
-	public boolean isEnRoute() {
-		// en Route 2014,21 15 2014 0000 0000 009
-		return this.number.startsWith("21") || 
-			   this.number.startsWith("2014");
-	}
-
+	/**
+	 * @return true if card is a JCB Card
+	 */
 	public boolean isJCB() {
 		// JCB 3088,3096,3112,3158,3337,3528 16 3088 0000 0000 0009
-		return this.number.startsWith("3088") || 
+		return  this.number.startsWith("3088") || 
 				this.number.startsWith("3096") || 
 				this.number.startsWith("3112") || 
 				this.number.startsWith("3158") || 
@@ -230,7 +234,7 @@ public class CreditCard {
 	public boolean isMaestro() {
 		// Maestro 5018, 5020, 5038, 5893, 6304, 6759, 6761, 6762, 6763 ;
 		// length 16-19
-		return this.number.startsWith("5018") || 
+		return  this.number.startsWith("5018") || 
 				this.number.startsWith("5020") || 
 				this.number.startsWith("5038") || 
 				this.number.startsWith("5893") || 
